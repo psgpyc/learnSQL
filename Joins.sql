f4e484b01cf2b3c0017acae1a8da0ae5f0fef2b7
paritosh666 <paritosh.ghimire666@gmail.com>
2023-10-01 11:03:10 +0100

    Added Queries to create database to be used in the tutorial

--- Joins.sql
+++ Joins.sql
@@ -17,3 +17,29 @@ ON  sql_hr.employees.office_id = sql_hr.offices.office_id;
 SELECT * FROM sql_hr.employees
 RIGHT JOIN sql_hr.offices
 ON sql_hr.employees.office_id = sql_hr.offices.office_id;
+
+-- SELECT ONLY REQUIRED COLUMN FROM THE JOIN 
+
+SELECT sql_hr.employees.first_name, sql_hr.employees.last_name, sql_hr.employees.job_title,
+	   sql_hr.offices.office_id, sql_hr.offices.address, sql_hr.offices.city, sql_hr.offices.state
+FROM sql_hr.employees
+JOIN sql_hr.offices
+ON sql_hr.employees.office_id = sql_hr.offices.office_id;
+
+-- JOIN WITH CONDITION
+
+SELECT sql_hr.employees.first_name, sql_hr.employees.last_name, sql_hr.employees.job_title,
+	   sql_hr.offices.office_id, sql_hr.offices.address, sql_hr.offices.city, sql_hr.offices.state
+FROM sql_hr.employees
+JOIN sql_hr.offices
+ON sql_hr.employees.office_id = sql_hr.offices.office_id
+WHERE sql_hr.offices.state IN ("NY" , "OH")
+ORDER BY sql_hr.offices.office_id DESC;
+
+
+-- GROUP BY WITH JOINS
+SELECT sql_hr.offices.state, sql_hr.offices.city ,AVG(sql_hr.employees.salary) FROM sql_hr.offices JOIN sql_hr.employees
+ON sql_hr.offices.office_id = sql_hr.employees.office_id
+GROUP BY sql_hr.offices.state, sql_hr.offices.city
+
+ 
